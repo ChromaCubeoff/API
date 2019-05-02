be0cdc4c0c93abd7ea6abee4e563ec02e0e1eae1
RealAlphaDEV <realalpha.ua@gmail.com>
2019-05-02 17:45:45 +0200

    Create DatabaseManager.java

---
+++ src/main/java/eu/chromacube/api/database/DatabaseManager.java
@@ -0,0 +1,16 @@
+package eu.chromacube.api.database;
+
+import eu.chromacube.api.API;
+
+public class DatabaseManager {
+
+    private API api;
+
+    public DatabaseManager(API api) {
+        this.api = api;
+    }
+
+    public void init(){
+
+    }
+}

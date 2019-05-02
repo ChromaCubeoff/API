6d0d5e15f796d69ee6aed06a65e84f34bd2e85f6
RealAlphaDEV <realalpha.ua@gmail.com>
2019-05-02 19:23:43 +0200

    Update API.java

--- src/main/java/eu/chromacube/api/API.java
+++ src/main/java/eu/chromacube/api/API.java
@@ -9,11 +9,11 @@ public class API extends JavaPlugin {
     private static API api;
     @Override
     public void onEnable() {
-        log("#==========[WELCOME TO CHROMA API]==========#");
-        log("# ChromaAPI is now loading. Please read     #");
-        log("# carefully all outputs coming from it.    #");
-        log("# by ChromaCube start the 02/05/2019      #");
-        log("#==========================================#");
+        log("#==========[WELCOME TO CHROMACUBE API]==========#");
+        log("#  ChromaAPI is now loading. Please read        #");
+        log("#  carefully all outputs coming from it.        #");
+        log("#  by ChromaCube start the 02/05/2019           #");
+        log("#===============================================#");
 
         api = this;
         super.onEnable();

4884c5e9f850bb8da53aa3ba29aeeb236da2e5c4
RealAlphaDEV <realalpha.ua@gmail.com>
2019-05-02 17:07:32 +0200

    Update API.java

--- src/main/java/eu/chromacube/api/API.java
+++ src/main/java/eu/chromacube/api/API.java
@@ -1,4 +1,20 @@
 package eu.chromacube.api;
 
-public class API {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class API extends JavaPlugin {
+
+    private static API api;
+    @Override
+    public void onEnable() {
+        api = this;
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    public static API get() { return api; }
 }

d31b4d87603c858fc4f0dfa5b0096457aa60fc07
RealAlphaDEV <realalpha.ua@gmail.com>
2019-05-02 17:45:41 +0200

    Update API.java

--- src/main/java/eu/chromacube/api/API.java
+++ src/main/java/eu/chromacube/api/API.java
@@ -2,11 +2,19 @@ package eu.chromacube.api;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class API extends JavaPlugin {
 
     private static API api;
     @Override
     public void onEnable() {
+        log("#==========[WELCOME TO CHROMA API]==========#");
+        log("# ChromaAPI is now loading. Please read     #");
+        log("# carefully all outputs coming from it.    #");
+        log("# by ChromaCube start the 02/05/2019      #");
+        log("#==========================================#");
+
         api = this;
         super.onEnable();
     }
@@ -17,4 +25,7 @@ public class API extends JavaPlugin {
     }
 
     public static API get() { return api; }
+    public void log(Level level, String msg) { this.getLogger().log(level, msg); }
+    public void log(String msg) { this.getLogger().info(msg); }
+
 }
